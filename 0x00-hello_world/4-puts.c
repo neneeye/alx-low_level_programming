482458ed4e1cbbdeabc2c21be1e23f6bf1408a49
neneeye <neneeye@gmail.com>
2022-09-08 20:32:53 +0000

    programmingyy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,9 @@
  * main - print the string in the put function
  *
  * Description - using the main function
- * this program teachs How to use Put to print a statement 
- * Return
+ * this program prints "Programming is like building a multilingual puzzle 
+ * Return:0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
