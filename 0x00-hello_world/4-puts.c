35bb2a8c7782841d945bbef7c1322391c2b433c3
neneeye <neneeye@gmail.com>
2022-09-08 20:38:00 +0000

    programmingyy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,7 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle 
- * Return:0
+ * this program prints
  */
 int main(void)
 {
