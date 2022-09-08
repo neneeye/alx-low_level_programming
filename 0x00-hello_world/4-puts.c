7e5c9ddf241bd83b359ac197fe0c2b3009da74f6
neneeye <neneeye@gmail.com>
2022-09-08 20:27:10 +0000

    programminggg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/* How to use Put to print a statement */
+/**
+ * main - print the string in the put function
+ *
+ * Description - using the main function
+ * this program teachs How to use Put to print a statement 
+ * Return: 0
+ */
 
 int main(void)
 {
