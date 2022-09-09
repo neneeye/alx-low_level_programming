96878a54179d6ee35de12c73cabde0006c646d0d
neneeye <neneeye@gmail.com>
2022-09-09 21:56:29 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++);
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if (ch != 'q' && ch != 'e');
+		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
 		}
