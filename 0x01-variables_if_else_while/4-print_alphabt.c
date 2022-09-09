c50540fa6c5e874002e013d3c3fe003e35c4eb63
neneeye <neneeye@gmail.com>
2022-09-09 21:19:53 +0000

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,18 @@
  * and print the last digit of the number stored in the variable n
  * *Return: Always 0 (Success)
  * **/
-
 int main(void)
 {
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++);
 	{
-			if (ch != 'q' && ch != 'e')
+			if (ch != 'q' && ch != 'e');
 		{
 			putchar(ch);
 		}
 	}
-
+	
 	putchar(10); /* this is an ascii code for new line*/
-
+	
 	return (0);
 }
