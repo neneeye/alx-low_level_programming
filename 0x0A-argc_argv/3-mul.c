4ea4cbd9c9a971c0f76661f84edcf936a1738cc1
neneeye <neneeye@gmail.com>
2022-09-30 22:25:10 +0000

    correcteddd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	
 	for (a = 0; a < argc; a++)
 	{
-		b = atoi(*(argv + 1)); /**b is first number*/
+		b = atoi( * (argv + 1)); /**b is first number*/
 		c = atoi(argv[2]); /** c is second number*/
 	}
 	product = b * c;
