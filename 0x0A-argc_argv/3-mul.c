e5c3009834361df36c83058108443111bf296e86
neneeye <neneeye@gmail.com>
2022-09-30 22:21:37 +0000

    correcteddd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,27 +7,23 @@
  * DESCRIPTION: outputs the product if 2 numbers
  * Return: Always (0) Success
  * **/
+
 int main(int argc, char *argv[])
 {
 	int a, b, c, product;
 	char error[10] = "Error";
-
 	if (argc != 3)
-
 	{
 		printf("%s\n", error);
-		
 		return (1);
 	}
-
+	
 	for (a = 0; a < argc; a++)
-
 	{
 		b = atoi(*(argv + 1)); /**b is first number*/
 		c = atoi(argv[2]); /** c is second number*/
 	}
 	product = b * c;
 	printf("%d\n", product);
-	
 	return (0);
 }
