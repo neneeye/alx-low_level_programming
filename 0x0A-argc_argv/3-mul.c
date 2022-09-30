f600cf7a40eab121d44a30a92e779f459304f657
neneeye <neneeye@gmail.com>
2022-09-30 22:17:22 +0000

    correcteddd

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - product of 2 numbers
+ * @argc: first parameter
+ * @argv: second parameter
+ * DESCRIPTION: outputs the product if 2 numbers
+ * Return: Always (0) Success
+ * **/
 int main(int argc, char *argv[])
 {
 	int a, b, c, product;
@@ -19,7 +26,7 @@ int main(int argc, char *argv[])
 		b = atoi(*(argv + 1)); /**b is first number*/
 		c = atoi(argv[2]); /** c is second number*/
 	}
-	product = b *c;
+	product = b * c;
 	printf("%d\n", product);
 	
 	return (0);
